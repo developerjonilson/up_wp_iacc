277c9024cf58b643e62c42ee60288cf395d245ae
jonilsondeveloper <jonilsondeveloper@gmail.com>
2018-04-04 17:52:05 -0300

    first commit

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <meta name="author" content="Lucas Peixoto" />
+        <meta name="author" content="UpMarketing Digital - Jonilson / Wacrksson" />
         <meta name="language" content="pt-br" />
         <meta name="keywords" content="" />
         <meta name="description" content="" />
